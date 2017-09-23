11fab1657ae6481c88c16cf73f2a317b728c607a
Oskar <oskarbank@gmail.com>
2017-09-23 12:20:36 +0200

    asd

--- src/main/java/pl/oskarpolak/testproject/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/testproject/controllers/MainController.java
@@ -6,16 +6,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import pl.oskarpolak.testproject.models.MysqlConnector;
 import pl.oskarpolak.testproject.models.UserSession;
 import pl.oskarpolak.testproject.models.dao.ContactDao;
 import pl.oskarpolak.testproject.models.dao.impl.ContactDaoImpl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Observable;
 import java.util.ResourceBundle;
@@ -24,10 +30,13 @@ public class MainController implements Initializable{
 
 
     @FXML
-    Label textNumber, textName;
+    TextField textNumber, textName;
 
     @FXML
     ListView<String> listContacts;
+
+    @FXML
+    Button buttonLogout;
 
     private ObservableList contactItems;
 
@@ -37,13 +46,26 @@ public class MainController implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
         loadContacts();
 
-        listContacts.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                textName.setText(newValue);
-                textNumber.setText(contactDao.getNumber(newValue));
-            }
+        listContacts.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            textName.setText(newValue);
+            textNumber.setText(contactDao.getNumber(newValue));
         });
+
+        buttonLogout.setOnMouseClicked(e -> logout());
+    }
+
+    private void logout() {
+        session.setLogedIn(false);
+        session.setUsername(null);
+        session.setId(0);
+
+        Stage stage = (Stage) buttonLogout.getScene().getWindow();
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("loginView.fxml"));
+            stage.setScene(new Scene(root, 600,400));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void loadContacts() {
